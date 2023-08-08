d360929bdc6b68e8b6fa58b54f55e56c71793427
milojezek <milekdeveloper@gmail.com>
2023-08-08 10:49:26 +0200

    Add list of places to home screen and their detail screens

---
+++ app/src/main/kotlin/app/futured/academyproject/ui/components/PlaceItem.kt
@@ -0,0 +1,119 @@
+package app.futured.academyproject.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import app.futured.academyproject.R
+import app.futured.academyproject.data.model.local.Place
+import app.futured.academyproject.tools.preview.PlacesProvider
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
+import kotlinx.collections.immutable.PersistentList
+
+@Composable
+fun PlaceItem(
+    place: Place,
+    modifier: Modifier = Modifier,
+    onClick: (Int) -> Unit = {},
+) {
+
+    Card(
+        modifier = modifier
+            .padding(R.dimen.small_padding.dp)
+            .fillMaxWidth()
+            .clickable {
+                onClick(place.id)
+            },
+        shape = RoundedCornerShape(
+            corner = CornerSize(R.dimen.rounded_corner.dp)
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = R.dimen.elevation.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(R.dimen.normal_padding.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+
+            PlaceImage(place = place)
+
+            Column(
+                modifier = Modifier
+                    .padding(R.dimen.small_padding.dp)
+            ) {
+                Text(
+                    text = place.name,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = place.type,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+
+
+        }
+    }
+
+}
+
+@Composable
+private fun PlaceImage(place: Place) {
+    Surface(
+        modifier = Modifier
+            .padding(R.dimen.normal_padding.dp)
+            .size(100.dp),
+        shape = CircleShape,
+        shadowElevation = R.dimen.elevation.dp,
+        color = Color.LightGray
+    ) {
+        val placeImagePainter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(place.image1Url)
+                .crossfade(true)
+                .transformations(CircleCropTransformation())
+                .build(),
+            contentScale = ContentScale.Crop
+        )
+
+        Image(
+            painter = placeImagePainter,
+            contentDescription = stringResource(R.string.place_image),
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun PlaceItemPreview(
+    @PreviewParameter(PlacesProvider::class) places: PersistentList<Place>
+) = Showcase {
+    PlaceItem(place = places.first(), onClick = {})
+}
